e641ea8d030a60558c2a8ad17b5987c8ae4a7725
Joel Yoder <github@joelyoder.com>
2023-09-07 03:58:15 -0400

    Added lots of game logic Turn taking is still not working...

--- script.js
+++ script.js
@@ -3,19 +3,27 @@
 // The Pot
 let gamePot = 0;
 let sabaccPot = 0;
+
+// Stacks of Cards
+let deck = new Array();
+let discard = new Array();
+
+// Defaults
+const startingCredits = 100;
+const gameAnte = 1;
+const sabaccAnte = 2;
+
+// Players
+const playerCount = 4;
+let players = new Array();
+
+// Game State
+let currentRound = 1;
+let currentStage = 1;
+let currentPlayer = 0;
 let currentBet = 0;
 
-// Record of Cards in Play
-let deck = new Array();
-let discardPile = new Array();
-
-// Player Stats
-let player1Hand = new Array();
-let player2Hand = new Array();
-
-let player1Credits = 0;
-let player2Credits = 0;
-
+// Set up a new game automatically on load
 function load()
 {
 	newGame();
@@ -23,29 +31,99 @@ function load()
 
 window.addEventListener('load', load);
 
-// Game Management
+// Game Setup
 function newGame() {
-    // Set up the deck
-    resetDeck();
-
     // Set everything to the defaults
-    player1Hand = new Array();
-    player2Hand = new Array();
     gamePot = 0;
     sabaccPot = 0;
+    currentPlayer = 0;
+    currentRound = 1;
+    currentStage = 1;
     currentBet = 0;
+
+    // Set up the players
+    for (let i = 0; i < playerCount; i++) {
+        players[i] = {Hand:new Array(), Credits:startingCredits};
+    }
+
+    updateGameUI();
+
+    newDeal();
+    anteUp();
+
+    console.log('Round ' + currentRound, 'Stage ' + currentStage, 'Player ' +  currentPlayer);
 }
 
 function endGame() {
+    // Determine winner via game conditions
+    // Assign winnings to the winning player
+    // Reset sabacc pot if they won via sabacc
 
+    // Reset everything that always should be reset
+    gamePot = 0;
+    currentPlayer = 0;
+    currentRound = 1;
+    currentStage = 1;
+    currentBet = 0;
+
+    // Empty the player hands
+    for (let i = 0; i < playerCount; i++) {
+        players[i] = {Hand:new Array()};
+    }
+
+    updateGameUI();
+
+    newDeal();
+    anteUp();
 }
+
+function updateGameUI() {
+    document.getElementById('gamePot').innerText = gamePot;
+    document.getElementById('sabaccPot').innerText = sabaccPot;
+    document.getElementById('currentBet').innerText = currentBet;
+    document.getElementById('currentRound').innerText = currentRound;
+    document.getElementById('currentPlayer').innerText = currentPlayer;
+    document.getElementById('currentRound').innerText = currentRound;
+
+    switch (currentStage) {
+        case 1:
+            document.getElementById('currentStage').innerText = 'Draw';
+            currentStage++;
+            break;
+        case 2:
+            document.getElementById('currentStage').innerText = 'Betting';
+            currentStage++;
+            break;
+        case 3:
+            document.getElementById('currentStage').innerText = 'Spike';
+            currentStage = 1;
+            currentRound++;
+            rollSpike();
+            break;
+        default:
+            document.getElementById('currentStage').innerText = 'Error';
+    }
+}
+
+// Turn Taking
+function nextPlayer() {
+    currentPlayer++;
+
+    if (currentPlayer === (playerCount - 1)) {
+        currentPlayer = 0;
+        console.log('reset player');
+    }
+
+    updateGameUI();
+}
+
 
 // The Card Deck
 const staves = ['circles', 'triangles', 'squares'];
 const values = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
 const sylopsNumber = 2;
 
-// Setting up the deck
+// Deck management
 function getDeck() {
     let deck = new Array();
 
@@ -87,7 +165,7 @@ function shuffle() {
 
 function renderDeck() {
 
-    // Get the deck element on the page
+    // Reset the element
     document.getElementById('deck').innerHTML = '';
 
     for(let i = 0; i < deck.length; i++) {
@@ -98,13 +176,11 @@ function renderDeck() {
         
         if (deck[i].Value > 0) {
             cardType = "pos";
-        } else if (deck[1].Value < 0) {
+        } else if (isNegative(deck[1].Value)) {
             cardType = "neg";
         } else {
             cardType = "";
         }
-
-        //console.log('this is working');
         
         // Create the divs for the cards and assign classes
         let card = document.createElement('div');
@@ -120,26 +196,53 @@ function renderDeck() {
 }
 
 function resetDeck() {
+    // Empty the discard pile first!
+    discard = new Array();
+
     deck = getDeck();
     shuffle();
 }
 
-// Card Management
-function dealToPlayer(player, cardAmount) {
+function discardDeck() {
+    discard.push(deck.pop());
 
+    document.getElementById('discard').innerText = discard[0].Value;
 }
 
-function addToHand(player, card) {
+function newDeal() {
+    resetDeck();
 
+    // Deal 2 cards to each player
+    for (let p = 0; p < playerCount; p++) {
+        players[p].Hand.push(...dealCards(2));
+    }
+
+    discardDeck();
 }
 
-function removeFromHand(player, card) {
+function reDeal() {
+    // Pull the current number of cards from each player
 
+    resetDeck();
+
+    // Deal out the same number of cards to each player
+
+    discardDeck();
+}
+
+function dealCards(cardNumber) {
+    let cards = new Array();
+
+    for (let i = 0; i < cardNumber; i++ ) {
+        cards.push(deck.pop());
+    }
+
+    return cards;
 }
 
 // Drawing Functions
 function stand() {
-
+    nextPlayer();
 }
 
 function drawFromPile() {
@@ -155,12 +258,33 @@ function swapFromDiscard() {
 }
 
 // Betting Functions
-function check() {
+function anteGamePot(player, amount) {
+    players[player].Credits = players[player].Credits - amount;
+    gamePot = gamePot + amount;
 
+    document.getElementById('gamePot').innerText = gamePot;
 }
 
-function bet(amount) {
+function anteSabaccPot(player, amount) {
+    players[player].Credits = players[player].Credits - amount;
+    sabaccPot = sabaccPot + amount;
 
+    document.getElementById('sabaccPot').innerText = sabaccPot;
+}
+
+function anteUp() {
+    for (let p = 0; p < playerCount; p++) {
+        anteGamePot(p, gameAnte);
+        anteSabaccPot(p, sabaccAnte);
+    }
+}
+
+function check() {
+    // Move to the next player's turn
+}
+
+function bet(pot, amount) {
+    
 }
 
 function call() {
@@ -182,4 +306,13 @@ function allIn() {
 // Spiking Functions
 function rollSpike() {
 
+}
+
+// Misc utilities
+function isNegative(num) {
+    if (typeof num === 'number' && Math.sign(num) === -1) {
+        return true;
+    }
+
+    return false;
 }
