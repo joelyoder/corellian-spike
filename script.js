f3ff6cae279867fa716ed5b76362f8385a4176bd
Joel Yoder <github@joelyoder.com>
2023-09-06 22:24:54 -0400

    Logic and controls scaffolding

---
+++ script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// The Pot
+let gamePot = 0;
+let sabaccPot = 0;
+let currentBet = 0;
+
+// Record of Cards in Play
+let drawPile = [];
+let discardPile = [];
+
+// Player Stats
+let player1Hand = [];
+let player2Hand = [];
+
+let player1Credits = 0;
+let player2Credits = 0;
+
+// Hand Management
+function addToHand(player, card) {
+
+}
+
+function removeFromHand(player, card) {
+
+}
+
+// Drawing Functions
+function stand() {
+
+}
+
+function drawFromPile() {
+
+}
+
+function swapFromPile() {
+
+}
+
+function swapFromDiscard() {
+
+}
+
+// Betting Functions
+function check() {
+
+}
+
+function bet(amount) {
+
+}
+
+function call() {
+
+}
+
+function raise(amount) {
+
+}
+
+function junk() {
+
+}
+
+function allIn() {
+
+}
+
+// Spiking Functions
+function rollSpike() {
+
+}
